68c7bed5b9616e4d4da6db05a8bebe4a045c3ab2
loki-patera <boulet.g@outlook.be>
2024-06-25 15:19:28 +0200

    Save datas in database and display them in views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,14 +1,40 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from listings.models import Band, Listing
+
 def hello(request):
-  return HttpResponse('<h1>Hello Django !</h1>')
+
+  bands  = Band.objects.all()
+
+  return HttpResponse(f"""
+    <h1>Hello Django !</h1>
+    <p>Mes groupes préférés sont :</p>
+    <ul>
+      <li>{bands[0].name}</li>
+      <li>{bands[1].name}</li>
+      <li>{bands[2].name}</li>
+    </ul>
+  """)
 
 def about(request):
+
   return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
 
 def listings(request):
-  return HttpResponse('<h1>Liste des annonces pour les articles</h1> <p>Première annonce</p>')
+
+  listings  = Listing.objects.all()
+
+  return HttpResponse(f"""
+    <h1>Liste des annonces pour les articles</h1>
+    <ul>
+      <li>{listings[0].title}</li>
+      <li>{listings[1].title}</li>
+      <li>{listings[2].title}</li>
+      <li>{listings[3].title}</li>
+    </ul>
+  """)
 
 def contact(request):
+
   return HttpResponse('<h1>Contactez-nous !</h1> <p>Voici nos coordonnées</p>')

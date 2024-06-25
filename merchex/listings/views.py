5dbdc25022b1b6d617cfd17973c23600415fca30
loki-patera <boulet.g@outlook.be>
2024-06-25 12:02:42 +0200

    Create views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+  return HttpResponse('<h1>Hello Django !</h1>')
+
+def about(request):
+  return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+  return HttpResponse('<h1>Liste des annonces pour les articles</h1> <p>Première annonce</p>')
+
+def contact(request):
+  return HttpResponse('<h1>Contactez-nous !</h1> <p>Voici nos coordonnées</p>')
